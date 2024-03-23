0af289edf2a3eda70d602a912b352dbbc33e614f
jakir10 <jakir.cse.bubt@gmail.com>
2024-03-24 03:01:31 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <>
+      <div>
+        <h1 className="text-xl text-teal-500">Bank System</h1>
+      </div>
+    </>
+  );
+}
+
+export default App;
---
+++ src/Pages/Home/Home.jsx
@@ -0,0 +1,9 @@
+const Home = () => {
+  return (
+    <div>
+      <h1>This is bank Home page</h1>
+    </div>
+  );
+};
+
+export default Home;
